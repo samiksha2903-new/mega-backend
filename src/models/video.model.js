f228c4e19310b723e177c89a4b2f3392e7859391
Samiksha Chaudhary <samiksha.choudhary2003@gmail.com>
2025-01-27 13:27:54 +0530

    Add comment, like, playlist and tweet models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -43,6 +43,6 @@ const videoSchema = new Schema(
     }
 )
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema)
